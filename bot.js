11f78c4d3c42ead59afe0f43b2d8b1a33a612b58
MDonChain <matthew.work094@gmail.com>
2026-03-18 17:47:07 +1100

    bot.js

--- bot.js
+++ bot.js
@@ -387,8 +387,7 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('DevDetector bot is running
-');
+  res.end('DevDetector bot is running');
 });
 
 server.listen(PORT, '0.0.0.0', () => {
